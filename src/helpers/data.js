38f9a54f3230594928e6401f574142cf904594bd
Manzaraz <magnusmanz@gmail.com>
2021-12-23 14:24:02 -0300

    adding manziflix

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -16,19 +16,24 @@
 export const products = [
   {
     id: 1,
+    img: "https://i.ibb.co/j3ZYjdH/screencapture-manziflix.png",
+    link: "https://manziflix.netlify.app/",
+  },
+  {
+    id: 2,
     img: "https://i.ibb.co/GP1zhtt/elementos-DOMejercicios.png",
     link: "https://manzaraz.github.io/DOM/",
   },
   {
-    id: 2,
+    id: 3,
     img: "https://i.ibb.co/v3nVyt7/FEMentor.png",
     link: "https://www.frontendmentor.io/solutions/mobile-first-html-css-bootstrap5-sass-_NV-UY3gw",
   },
-  {
-    id: 3,
-    img: "https://i.ibb.co/3CtQ8Gp/3.jpg",
-    link: "http://lama.dev",
-  },
+  // {
+  //   id: 4,
+  //   img: "https://i.ibb.co/v3nVyt7/FEMentor.png",
+  //   link: "https://www.frontendmentor.io/solutions/mobile-first-html-css-bootstrap5-sass-_NV-UY3gw",
+  // },
 ];
 
 export const techs = [
